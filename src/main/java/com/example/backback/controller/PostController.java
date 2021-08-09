bda99b80127cd4eed701683535778a2eaa974111
rambaldi <diencong5998@gmail.com>
2021-08-09 15:03:33 +0700

    sua like

--- src/main/java/com/example/backback/controller/PostController.java
+++ src/main/java/com/example/backback/controller/PostController.java
@@ -115,7 +115,7 @@ public class PostController {
         if(!userPost.isPresent())
             return new ResponseEntity<>(new ResponMessage("khong tim thay "),HttpStatus.BAD_REQUEST);
 
-        // tim trong sv
+
         List postList =  postService.findByUser(userPost.get());
         if(postList.isEmpty())
             return new ResponseEntity<>(new ResponMessage("khong co bai post nao"),HttpStatus.BAD_REQUEST);
@@ -147,8 +147,7 @@ public class PostController {
                 likeCommentService.deleteAllByCommentPost((CommentPost) commentPost);
                 commentPostService.delete((CommentPost) commentPost);
             }
-            // xoa
-//            commentPostService.deleteAllByPost(post.get());
+
 
 
             postService.remove(id);
